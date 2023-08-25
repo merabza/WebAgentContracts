574985f5afcfbbd508efb7e7ffb9fdb66e36495a
merabza <merabza@gmail.com>
2023-08-25 21:39:34 +0400

    202308252139

---
+++ WebAgentMessagesContracts/IMessagesDataManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace WebAgentMessagesContracts;
+
+public interface IMessagesDataManager
+{
+    Task SendMessage(string? userName, string message);
+    void UserConnected(string connectionId, string userName);
+    void UserDisconnected(string connectionId, string userName);
+}
