ba8c716d351c8c209651c91f86351cd90e8397b0
merabza <merabza@gmail.com>
2023-08-31 14:03:01 +0400

    202308311402

--- WebAgentDatabasesApiContracts/V1/Routes/DatabaseApiRoutes.cs
+++ WebAgentDatabasesApiContracts/V1/Routes/DatabaseApiRoutes.cs
@@ -8,33 +8,33 @@ public static class DatabaseApiRoutes
 
     public static class Database
     {
-        public const string DatabaseBase = Base + "/database";
+        public const string DatabaseBase = Base + "/databases";
 
-        //// POST api/database/checkrepairdatabase/{databaseName}
+        //// POST api/databases/checkrepairdatabase/{databaseName}
         public const string CheckRepairDatabase = "/checkrepairdatabase/{databaseName}";
 
-        //// POST api/database/createbackup/{databaseName}
+        //// POST api/databases/createbackup/{databaseName}
         public const string CreateBackup = "/createbackup/{databaseName}";
 
-        //// POST api/database/executecommand/{databaseName}
+        //// POST api/databases/executecommand/{databaseName}
         public const string ExecuteCommand = "/executecommand/{databaseName}";
 
-        //// GET api/database/getdatabasenames
+        //// GET api/databases/getdatabasenames
         public const string GetDatabaseNames = "/getdatabasenames";
 
-        //// GET api/database/isdatabaseexists/{databaseName}
+        //// GET api/databases/isdatabaseexists/{databaseName}
         public const string IsDatabaseExists = "/isdatabaseexists/{databaseName}";
 
-        //// PUT restorebackup/{databaseName}
+        //// PUT api/databases/restorebackup/{databaseName}
         public const string RestoreBackup = "/restorebackup/{databaseName}";
 
-        //// POST api/database/recompileprocedures/{databaseName}
+        //// POST api/databases/recompileprocedures/{databaseName}
         public const string RecompileProcedures = "/recompileprocedures/{databaseName}";
 
-        //// GET api/database/testconnection/{databaseName?}
+        //// GET api/databases/testconnection/{databaseName?}
         public const string TestConnection = "/testconnection/{databaseName?}";
 
-        //// POST api/database/updatestatistics/{databaseName}
+        //// POST api/databases/updatestatistics/{databaseName}
         public const string UpdateStatistics = "/updatestatistics/{databaseName}";
     }
 }
