25c7b1206041b57ccd2cebd4b3e52f5ea452ac91
merabza <merabza@gmail.com>
2024-04-22 20:29:02 +0400

    202404222028

--- WebAgentProjectsApiContracts/V1/Requests/RestoreBackupRequest.cs
+++ WebAgentProjectsApiContracts/V1/Requests/RestoreBackupRequest.cs
@@ -6,4 +6,6 @@ public sealed class RestoreBackupRequest
     public string? Suffix { get; set; }
     public string? Name { get; set; }
     public string? DateMask { get; set; }
+    public string? DestinationDbServerSideDataFolderPath { get; set; }
+    public string? DestinationDbServerSideLogFolderPath { get; set; }
 }
