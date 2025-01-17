0fdfe1337cac3dd09bcaca7c32fb19e02d1811b2
merabza <merabza@gmail.com>
2025-01-17 20:27:47 +0400

    202501172024

--- WebAgentDatabasesApiContracts/Errors/DatabaseApiClientErrors.cs
+++ WebAgentDatabasesApiContracts/Errors/DatabaseApiClientErrors.cs
@@ -6,7 +6,6 @@ public static class DatabaseApiClientErrors
 {
     public static readonly Err AppSettingsIsNotCreated = new()
     {
-        ErrorCode = nameof(AppSettingsIsNotCreated),
-        ErrorMessage = "appSettings is not created"
+        ErrorCode = nameof(AppSettingsIsNotCreated), ErrorMessage = "appSettings is not created"
     };
 }
