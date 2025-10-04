fb371329937ac38fb938a0ab57de4cc24acb1d51
merabza <merabza@gmail.com>
2025-10-04 14:52:45 +0400

    202510041452

---
+++ WebAgentDatabasesApiContracts/V1/Requests/CreateDatabaseBackupRequest.cs
@@ -0,0 +1,14 @@
+﻿using DbTools;
+
+namespace WebAgentDatabasesApiContracts.V1.Requests;
+
+public sealed class CreateDatabaseBackupRequest
+{
+    public string BackupNamePrefix { get; init; } = null!;
+    public string DateMask { get; init; } = null!;
+    public string BackupFileExtension { get; init; } = null!;
+    public string BackupNameMiddlePart { get; init; } = null!;
+    public bool Compress { get; init; } = false;
+    public bool Verify { get; init; } = false;
+    public EBackupType BackupType { get; init; } = EBackupType.Full;
+}
