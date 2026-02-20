6815d7dfd7ee83891241f52c8c1a94654ce336bc
merabza <merabza@gmail.com>
2026-02-20 13:13:36 +0400

    202602201313

--- WebAgentContracts.WebAgentDatabasesApiContracts/V1/Requests/CreateDatabaseBackupRequest.cs
+++ WebAgentContracts.WebAgentDatabasesApiContracts/V1/Requests/CreateDatabaseBackupRequest.cs
@@ -8,7 +8,7 @@ public sealed class CreateDatabaseBackupRequest
     public string DateMask { get; init; } = null!;
     public string BackupFileExtension { get; init; } = null!;
     public string BackupNameMiddlePart { get; init; } = null!;
-    public bool Compress { get; init; } = false;
-    public bool Verify { get; init; } = false;
+    public bool Compress { get; init; }
+    public bool Verify { get; init; }
     public EBackupType BackupType { get; init; } = EBackupType.Full;
 }
